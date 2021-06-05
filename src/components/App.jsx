a438758d910840e416f1a0cd3780ece56a228764
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-05 22:44:53 +0530

    Method1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [fname , setfName] = useState("");
+  const [lname , setlName] = useState("");
+ 
+
+  function handleFName(event){
+    setfName(event.target.value)
+  }
+  function handleLName(event){
+    setlName(event.target.value)
+  }
+
+  function handleClick(event){
+    event.preventDefault(); 
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>Hello {fname} {lname}</h1>
+      <form onSubmit={handleClick}>
+        <input 
+          name="fName" 
+          onChange={handleFName} 
+          placeholder="First Name"
+          value = {fname} />
+        <input 
+          name="lName" 
+          onChange={handleLName} 
+          placeholder="Last Name"
+          value = {lname} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
