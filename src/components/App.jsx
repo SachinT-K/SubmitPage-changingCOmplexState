690d571a2554539ac229f325edc55c06eda47543
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-06 11:57:25 +0530

    Adding email and setContact holding previous values of other fields.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fullName, setfullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
-    lName: ""
+    lName: "",
+    email: ""
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
     // name is event.target.name and value is event.target.value
-    setfullName((prevValue) => {
+    setContact((prevValue) => {
       if (name === "fName") {
         return {
           fName: value,
-          lName: prevValue.lName
+          lName: prevValue.lName,
+          email: prevValue.email
         };
       } else if (name === "lName") {
         return {
           fName: prevValue.fName,
-          lName: value
+          lName: value,
+          email: prevValue.email
         };
+      } else if (name === "email"){
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        }
       }
     });
   }
@@ -31,20 +40,27 @@ function App() {
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form onSubmit={handleClick}>
         <input
           name="fName"
           onChange={handleChange}
           placeholder="First Name"
-          value={fullName.fName}
+          value={contact.fName}
         />
         <input
           name="lName"
           onChange={handleChange}
           placeholder="Last Name"
-          value={fullName.lName}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          onChange={handleChange}
+          placeholder="Email"
+          value={contact.email}
         />
         <button type="submit">Submit</button>
       </form>
